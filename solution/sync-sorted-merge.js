be8ac97af9eecc63aaf5c6b79dbf93709fd0e7f3
Aaron Dai <mrvicadai@gmail.com>
2024-06-12 22:29:14 -0400

    implement sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,26 @@
 "use strict";
 
+const PriorityQueue = require("../lib/pq");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const store = new PriorityQueue();
+
+  for (let i = 0, logSource; i < logSources.length; i++) {
+    logSource = logSources[i];
+    // Use the date as the key for priority comparison
+    store.enqueue(logSource, logSource.last.date);
+  }
+
+  let currentLogSource, nextLogOrDrained;
+  while (!store.isEmpty()) {
+    currentLogSource = store.dequeue();
+    printer.print(currentLogSource.last);
+    nextLogOrDrained = currentLogSource.pop();
+    if (!nextLogOrDrained) {
+      continue;
+    }
+    store.enqueue(currentLogSource, nextLogOrDrained.date);
+  }
 };
