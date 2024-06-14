414671b7e73632c111f5298beb676657ae68cc96
Aaron Dai <mrvicadai@gmail.com>
2024-06-13 22:13:14 -0400

    improve perf of async solution by using caching

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,15 +1,16 @@
 "use strict";
 
 const PriorityQueue = require("../lib/pq");
+const CachedLogSource = require("../lib/cached-log-source");
 
 // Print all entries, across all of the *async* sources, in chronological order.
 module.exports = (logSources, printer) => {
   const store = new PriorityQueue();
 
   for (let i = 0, logSource; i < logSources.length; i++) {
-    logSource = logSources[i];
+    logSource = new CachedLogSource(logSources[i]);
     // Use the date as the key for priority comparison
-    store.enqueue(logSource, logSource.last.date);
+    store.enqueue(logSource, logSource.peek().date);
   }
 
   function next() {
@@ -21,11 +22,12 @@ module.exports = (logSources, printer) => {
       }
 
       const logSource = store.dequeue();
-      printer.print(logSource.last);
+      printer.print(logSource.peek());
 
       logSource
         .popAsync()
-        .then((logOrDrained) => {
+        .then(() => {
+          let logOrDrained = logSource.peek();
           if (logOrDrained !== false) {
             store.enqueue(logSource, logOrDrained.date);
           }
